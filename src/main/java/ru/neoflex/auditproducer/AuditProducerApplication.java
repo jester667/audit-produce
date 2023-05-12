5201e90c1e764ee3da59cbc80d9861444f3492d2
Томко Владислав Викторович <tomko@dev.vtb.ru>
2023-05-12 10:51:32 +0300

    delete test

--- src/main/java/ru/neoflex/auditproducer/AuditProducerApplication.java
+++ src/main/java/ru/neoflex/auditproducer/AuditProducerApplication.java
@@ -2,8 +2,10 @@ package ru.neoflex.auditproducer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
+@EnableKafka
 public class AuditProducerApplication {
 
     public static void main(String[] args) {
